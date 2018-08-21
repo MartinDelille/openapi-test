0ed3fee68814f75890657ae3782160d837ef387b
Martin Delille <martin@phonations.com>
2018-08-21 13:13:13 +0200

    create alias for java environment

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
+OS=$(uname -s);
+if [ $OS != "Darwin" ]; then
+  echo "Creating alias"
+  alias openapi-generator -jar openapi-generator-cli.jar
+else
+  echo "no alias for macOS"
+fi
 openapi-generator generate -i api.yaml -g html -o html
+
